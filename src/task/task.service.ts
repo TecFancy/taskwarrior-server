21946505ef656dec2b19e6db6d9ee08f403f76b1
smpower <rf.wangchn@foxmail.com>
2023-03-03 00:28:04 +0800

    feat: 优化 getTaskBySingleTag 方法

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -33,9 +33,13 @@ export class TaskService {
   async getTaskBySingleTag(tag: string): Promise<Task[]> {
     const taskWarriorProcess = spawn('task', [
       // rc.confirmation=no，rc.recurrence.confirmation=no 和 rc.dependency.confirmation=no 可以关闭 Task Warrior 的确认提示
+      'rc.confirmation=no',
+      'rc.recurrence.confirmation=no',
+      'rc.dependency.confirmation=no',
       // rc.json.depends.array=yes 可以让 Task Warrior 在导出 JSON 数据时包含依赖任务信息
+      'rc.json.depends.array=yes',
       // rc.bulk=0 可以禁用 Task Warrior 的批量操作
-      'rc.confirmation=no rc.recurrence.confirmation=no rc.dependency.confirmation=no rc.json.depends.array=yes rc.buld=0',
+      'rc.buld=0',
       `tag:${tag}`,
       'export',
     ]);
