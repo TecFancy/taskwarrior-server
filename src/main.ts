2f2e7b91418ccaa3158629f76716a79640c59b0a
smpower <rf.wangchn@foxmail.com>
2023-03-02 14:09:29 +0800

    feat: 应用程序级别添加统一接口前缀 api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // 所有接口统一前缀
   await app.listen(3000);
 }
 bootstrap();
