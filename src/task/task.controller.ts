13ef98c6633b5614ce8ea1a5d38f73f3cd54dcbb
smpower <rf.wangchn@foxmail.com>
2023-03-05 02:45:09 +0800

    feat: 查询任务公共逻辑提取

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -8,18 +8,18 @@ export class TaskController {
 
   @Get('all')
   async getAllTasks(): Promise<Task[]> {
-    return await this.taskService.getAllTasks();
+    return await this.taskService.getTasks();
   }
 
   @Get('/tag/:tag')
   async getTasksBySingleTag(@Param('tag') tag: string): Promise<Task[]> {
-    return await this.taskService.getTasksBySingleTag(tag);
+    return await this.taskService.getTasks(`tag:${tag}`);
   }
 
   @Get('/project/:project')
   async getTasksBySingleProject(
     @Param('project') project: string,
   ): Promise<Task[]> {
-    return await this.taskService.getTasksBySingleProject(project);
+    return await this.taskService.getTasks(`project:${project}`);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -5,19 +5,22 @@ import { Task } from './task.model';
 
 @Injectable()
 export class TaskService {
-  private getTasks(params: string): Promise<Task[]> {
-    const taskWarriorProcess = spawn('task', [
-      // rc.confirmation=no，rc.recurrence.confirmation=no 和 rc.dependency.confirmation=no 可以关闭 Task Warrior 的确认提示
-      'rc.confirmation=no',
-      'rc.recurrence.confirmation=no',
-      'rc.dependency.confirmation=no',
-      // rc.json.depends.array=yes 可以让 Task Warrior 在导出 JSON 数据时包含依赖任务信息
-      'rc.json.depends.array=yes',
-      // rc.bulk=0 可以禁用 Task Warrior 的批量操作
-      'rc.buld=0',
-      `${params}`,
-      'export',
-    ]);
+  async getTasks(params?: string): Promise<Task[]> {
+    const taskArgs = params?.length
+      ? [
+          // rc.confirmation=no，rc.recurrence.confirmation=no 和 rc.dependency.confirmation=no 可以关闭 Task Warrior 的确认提示
+          'rc.confirmation=no',
+          'rc.recurrence.confirmation=no',
+          'rc.dependency.confirmation=no',
+          // rc.json.depends.array=yes 可以让 Task Warrior 在导出 JSON 数据时包含依赖任务信息
+          'rc.json.depends.array=yes',
+          // rc.bulk=0 可以禁用 Task Warrior 的批量操作
+          'rc.buld=0',
+          `${params}`,
+          'export',
+        ]
+      : ['export'];
+    const taskWarriorProcess = spawn('task', taskArgs);
     return new Promise((resolve, reject) => {
       let result = '';
       let error = '';
@@ -36,39 +39,5 @@ export class TaskService {
         } else reject(error);
       });
     });
-  }
-
-  /** 查询所有任务 */
-  async getAllTasks(): Promise<Task[]> {
-    const taskWarriorProcess = spawn('task', ['export']); // 通过 "task export" 命令获取所有任务
-
-    return new Promise((resolve, reject) => {
-      let result = '';
-      let error = '';
-
-      taskWarriorProcess.stdout.on('data', (data) => {
-        result += data.toString();
-      });
-
-      taskWarriorProcess.stderr.on('data', (data) => {
-        error += data.toString();
-      });
-
-      taskWarriorProcess.on('close', (code) => {
-        if (code === 0) {
-          resolve(JSON.parse(result));
-        } else reject(error);
-      });
-    });
-  }
-
-  /** 根据一个 tag 查询某些任务 */
-  async getTasksBySingleTag(tag: string): Promise<Task[]> {
-    return this.getTasks(`tag:${tag}`);
-  }
-
-  /** 根据一个 project 查询某些任务 */
-  async getTasksBySingleProject(project: string): Promise<Task[]> {
-    return this.getTasks(`project:${project}`);
   }
 }
